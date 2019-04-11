2df406fdb9a33b63592320c5508659e98be03ccd
Jon Felix Rico <jonfelixrico@gmail.com>
2019-04-11 23:59:01 +0800

    [BE] Development on Seminar controllers

--- backend/src/controllers/seminar.controller.ts
+++ backend/src/controllers/seminar.controller.ts
@@ -1,10 +1,39 @@
 import * as Seminar from '../models/seminar.model';
 
-export async function create(req: any, res: any) {
-    try {
-        let seminar = await Seminar.create(req.user._id, req.body);
-        res.send(seminar.toObject());
-    } catch (err) {
-        res.status(400).send(err);
+export namespace organizer {
+    export async function create(req: any, res: any) {
+        try {
+            let seminar = await Seminar.create(req.user._id, req.body);
+            res.status(210).send(seminar.toObject());
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    }
+
+    export async function select(req: any, res: any) {
+        try {
+            let seminar = await Seminar.select(req.params.seminarId);
+            await Seminar.collaborators.verify(seminar, req.user._id);
+
+            let obj = seminar.toObject();
+            delete obj.attendeeFields;
+            delete obj.certificateFields;
+
+            res.send(obj);
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    }
+}
+
+export namespace guest {
+    export async function select(req: any, res: any) {
+        try {
+            let seminar = await Seminar.select(req.params.seminarId);
+
+            res.send(seminar.toObject().info);
+        } catch (err) {
+            res.status(400).send(err);
+        }
     }
 }
