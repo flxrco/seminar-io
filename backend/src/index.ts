6c8bf3294b3c97c2a6794945b44efb18396aa8fa
Jon Felix Rico <jonfelixrico@gmail.com>
2019-04-17 19:22:42 +0800

    [BE] Integrate authentication module with the app
    - Installed the express-session package.
    - Utilized crypto for the express-session secret option.
    - Passport has been initialized.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,13 @@
 import express = require('express');
 import { json, urlencoded } from 'express';
+
+import session = require('express-session');
+import { randomBytes } from 'crypto';
+
+import { passport } from './utils/passport.util';
+
 import { routes } from './routes/index.route';
+
 import { connect } from 'mongoose';
 
 connect('mongodb://localhost/seminar-io', { useNewUrlParser: true });
@@ -9,11 +16,25 @@ console.log('MongoDB-Mongoose connection successful.');
 const app = express();
 const SERVER_PORT = 3000;
 
+// parsers and stuff
 app.use(json());
-app.use(urlencoded({
-    extended: true
+app.use(urlencoded({ extended: true }));
+
+// session support
+app.use(session({ 
+    secret: randomBytes(48).toString('base64'),
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        secure: false
+    }
 }));
 
+// passport stuff
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
 app.use('', routes);
 
 app.listen(SERVER_PORT, () => {
